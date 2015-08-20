8c1286dcdc172603b4912f5a899eb26161141fc2
Karla Hernandez <karla.hernandez0728@gmail.com>
2015-08-19 19:59:00 -0500

    ASP Blank app

---
+++ AppointmentScheduler/AppointmentScheduler/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppointmentScheduler.Startup))]
+namespace AppointmentScheduler
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
